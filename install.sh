c6aef2018adc7b10615927ff1bab2e99da89e9cc
Bluesky <blueskyusf@gmail.com>
2026-04-14 14:05:01 -0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 echo Hello to S Install!
 sleep 2
 echo "Data Install in: https://github.com/blueskyusf-code/SunTool/raw/main/SunTool-Bin-1.0.zip"
-curl -L https://github.com/blueskyusf-code/SunTool/raw/main/SunTool-Bin-1.0.zip -o SunTool-Bin-1.0.zip
+curl -L https://github.com/blueskyusf-code/SunTool/raw/main/SunTool-Bin-1.0.zip -o SunTool-Bin-1.0.zip > /dev/null
 echo extracting file...
-unzip SunTool-Bin-1.0.zip 2>/dev/null
-rmdir META-INF 2>/dev/null
+unzip SunTool-Bin-1.0.zip > /dev/null
+rmdir META-INF > /dev/null 
 mv SunTool/SunTool $PREFIX/bin/
 shred -u KSun.xml
 chmod +x $PREFIX/bin/SunTool
